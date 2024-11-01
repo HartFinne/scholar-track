ce1470759b109ca69efce6f414101ea4434dccb1
ScholarTrackPH <inquiriescholartrack@gmail.com>
2024-11-01 22:06:30 +0800

    Update evaluate_scholars.py

--- storage/app/python/evaluate_scholars.py
+++ storage/app/python/evaluate_scholars.py
@@ -8,7 +8,7 @@ import json
 # Database connection details
 DB_CONNECTION = 'mysql'
 DB_HOST = '127.0.0.1'
-DB_PORT = '3306'
+DB_PORT = 3306
 DB_DATABASE = 'laravel'
 DB_USERNAME = 'forge'
 DB_PASSWORD = 'Xf2DrGKSxX02BJ1S5yxI'
