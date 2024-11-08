d2785a0d4f4873c22f47c3ea3d2a742301e0067e
eyyiii <jeyempleo@gmail.com>
2024-11-08 15:54:50 +0800

    fix lte

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Middleware\applicant;
 use Illuminate\Http\Request;
-use App\Models\user;
+use App\Models\User;
 use App\Models\staccount;
 use App\Models\applicants;
 use App\Notifications\ResetPassword;
