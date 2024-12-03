718dc4cdedf402dfadb20b104c885e4ac1231613
HartFinne <167497706+HartFinne@users.noreply.github.com>
2024-12-04 07:01:51 +0800

    Update PDFController.php

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -109,6 +109,7 @@ class PDFController extends Controller
         $pdf = Browsershot::html($template)
             ->setNodeBinary('/usr/bin/node')
             ->setNpmBinary('/usr/bin/npm')
+            ->useBinary('/usr/bin/chromium-browser')
             ->showBackground()
             ->margins(2, 4, 10, 4)
             ->format('Letter')
