8c8627958f6b2877a7e235735a7c7504ecb54f9e
HartFinne <167497706+HartFinne@users.noreply.github.com>
2024-12-04 06:52:39 +0800

    Update PDFController.php

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -107,8 +107,8 @@ class PDFController extends Controller
 
         // Generate the PDF using Browsershot and save it
         $pdf = Browsershot::html($template)
-            ->setNodeBinary('/usr/local/bin/node')
-            ->setNpmBinary('/usr/local/bin/npm')
+            ->setNodeBinary('/usr/bin/node')
+            ->setNpmBinary('/usr/bin/npm')
             ->showBackground()
             ->margins(2, 4, 10, 4)
             ->format('Letter')
